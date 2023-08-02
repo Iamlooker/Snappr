b998819f6b8c31771f3d5e8869ecdaf36e2ef43b
LooKeR <mohit2002ss@gmail.com>
2023-08-02 10:34:46 +0530

    Remove unused classes

--- app/src/main/java/com/looker/notesy/ui/utils/ImageUtils.kt
+++ app/src/main/java/com/looker/notesy/ui/utils/ImageUtils.kt
@@ -2,17 +2,7 @@ package com.looker.notesy.ui.utils
 
 import android.content.Context
 import android.graphics.Bitmap
-import androidx.collection.LruCache
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.Stable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.core.graphics.drawable.toBitmap
 import androidx.palette.graphics.Palette
 import coil.imageLoader
@@ -22,45 +12,6 @@ import coil.request.SuccessResult
 import coil.size.Scale
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-
-@Composable
-fun rememberDominantColorState(
-	context: Context = LocalContext.current,
-	defaultColor: Color = MaterialTheme.colorScheme.surface,
-	cacheSize: Int = 12,
-): DominantColorState = remember { DominantColorState(context, defaultColor, cacheSize) }
-
-@Stable
-class DominantColorState(
-	private val context: Context,
-	private val defaultColor: Color,
-	cacheSize: Int = 50,
-) {
-	var color by mutableStateOf(defaultColor)
-		private set
-
-	private val cache = when {
-		cacheSize > 0 -> LruCache<String, DominantColors>(cacheSize)
-		else -> null
-	}
-
-	suspend fun updateColorsFromImageUrl(url: String?) {
-		val result = calculateDominantColorFromUrl(url)
-		color = result?.color ?: defaultColor
-	}
-
-	private suspend fun calculateDominantColorFromUrl(url: String?): DominantColors? {
-		return if (url != null) {
-			cache?.get(url)
-				?: context.calculateColorFromImageUrl(url)?.let { dominantColor ->
-					DominantColors(dominantColor).also { result -> cache?.put(url, result) }
-				}
-		} else null
-	}
-}
-
-@Immutable
-data class DominantColors(val color: Color = Color.Unspecified)
 
 suspend fun Context.calculateColorFromImageUrl(
 	imageUrl: String,
